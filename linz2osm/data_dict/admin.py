67d6459f963ba953941fbfba63390b7f480d921f
Robert Coup <robert@coup.net.nz>
2010-04-19 10:02:19 +1200

    Add layer-stats link in layer list

--- linz2osm/data_dict/admin.py
+++ linz2osm/data_dict/admin.py
@@ -9,7 +9,7 @@ class TagInline(admin.StackedInline):
     verbose_name_plural = 'Tags'
 
 class LayerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'entity', 'get_geometry_type_display', 'tag_count', 'datasets', 'notes',)
+    list_display = ('name', 'entity', 'get_geometry_type_display', 'stats_link', 'tag_count', 'datasets', 'notes',)
     list_filter = ('entity',)
     inlines = [
         TagInline,
@@ -33,6 +33,11 @@ class LayerAdmin(admin.ModelAdmin):
         ds.sort()   # make them in a consistent order
         return ", ".join(ds)
     datasets.short_description = 'Available in' 
+
+    def stats_link(self, obj):
+        return "<a href='%s/stats/'>Layer Stats</a>" % obj.name
+    stats_link.short_description = 'Statistics'
+    stats_link.allow_tags = True
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ('tag',)
