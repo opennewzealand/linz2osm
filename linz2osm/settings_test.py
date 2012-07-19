99119560bab27a0531a102373179f49e9c96f913
Stephen Davis <steve@koordinates.com>
2012-07-19 13:57:16 +1200

    fix tests for Celery: closes #27

--- linz2osm/settings_test.py
+++ linz2osm/settings_test.py
@@ -12,23 +12,17 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE' : 'django.contrib.gis.db.backends.postgis',
-        'NAME' : 'test_linz2osm',
+        'NAME' : 'linz2osm_test',
     },
 }
 
 INSTALLED_APPS = (
-    # 'django.contrib.auth',
-    # 'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
-    # 'django.contrib.messages',
-    # 'django.contrib.admin',
-    # 'django.contrib.admindocs',
-    # 'django.contrib.staticfiles',
-    # 'django.contrib.gis',
-    
     'linz2osm.data_dict',
     'linz2osm.convert',
-    'linz2osm.workslices',
+    # 'linz2osm.workslices',
     
+    # 'djcelery',
     'south',
 )
+
+BROKER_URL = 'amqp://guest@localhost//'
