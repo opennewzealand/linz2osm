d0214d2e32c6965a3f4f4cd1a57aaa225e652aee
Stephen Davis <steve@koordinates.com>
2012-07-19 17:29:02 +1200

    add columns for Workslice admin

--- linz2osm/workslices/admin.py
+++ linz2osm/workslices/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 from linz2osm.workslices.models import Workslice
 
-# class WorksliceAdmin(admin.ModelAdmin):
-#     pass
 
-admin.site.register(Workslice)
+class WorksliceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'state', 'checked_out_at', 'layer', 'user', 'dataset',)
+    ordering = ('id',)
+    readonly_fields = ('name', 'checked_out_at', 'status_changed_at', 'layer', 'dataset', 'bounds',)
 
+admin.site.register(Workslice, WorksliceAdmin)
