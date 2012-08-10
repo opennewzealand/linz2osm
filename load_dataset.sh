9671901392c9ba58ff9e75989bbd82c1ef627414
Stephen Davis <steve@koordinates.com>
2012-08-10 17:25:05 +1200

    fix SRIDs for load_dataset.sh

--- load_dataset.sh
+++ load_dataset.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
-    echo "Usage: load_dataset.sh <directory1> [<directory2> ...]"
+    echo "Usage: load_dataset.sh <directory1> <srid>"
     exit 1
 fi
 
-for dataset_dir in "$@"
-do
-    dataset_base=`basename ${dataset_dir}`
-    echo "Loading directory: ${dataset_base}"
-    dataset_db=`echo ${dataset_base} | sed -e 's/\(.\)/\L\1/g' -e 's/\([^a-z0-9]\)/_/g'`
-    echo "Will use database ${dataset_db}"
-    dropdb ${dataset_db}
-    createdb ${dataset_db} -T template_postgis
+dataset_base=`basename $1`
+echo "Loading directory: ${dataset_base}"
+dataset_db=`echo ${dataset_base} | sed -e 's/\(.\)/\L\1/g' -e 's/\([^a-z0-9]\)/_/g'`
+echo "Will use database ${dataset_db}"
+dropdb ${dataset_db}
+createdb ${dataset_db} -T template_postgis
 
-    for shapefile in ${dataset_dir}/*.shp
-    do
-        if [ "`basename ${shapefile}`" = "contour.shp" ]
-        then
-            continue
-        fi
-        echo "${shapefile}"
-        ogr2ogr -overwrite -f PostgreSQL PG:dbname=${dataset_db} -lco OVERWRITE=yes -lco LAUNDER=yes ${shapefile}
-    done
+for shapefile in $1/*.shp
+do
+    if [ "`basename ${shapefile}`" = "contour.shp" ]
+    then
+        continue
+    fi
+    echo "${shapefile}"
+    ogr2ogr -overwrite -f PostgreSQL PG:dbname=${dataset_db} -lco OVERWRITE=yes -lco LAUNDER=yes ${shapefile} -a_srs $2 -s_srs $2
 done
