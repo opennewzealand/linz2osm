e2079bbc65b7e4b9b8e6830f48472e67ea3053c1
Stephen Davis <steve@koordinates.com>
2012-08-13 13:28:38 +1200

    workslice admin make fields readonly

--- linz2osm/workslices/admin.py
+++ linz2osm/workslices/admin.py
@@ -6,7 +6,7 @@ from linz2osm.workslices.models import Workslice
 class WorksliceAdmin(admin.ModelAdmin):
     list_display = ('name', 'state', 'checked_out_at', 'layer', 'user', 'dataset',)
     ordering = ('id',)
-    readonly_fields = ('name', 'checked_out_at', 'status_changed_at', 'layer_in_dataset',)
+    readonly_fields = ('name', 'checked_out_at', 'status_changed_at', 'layer_in_dataset','checkout_extent','feature_count', 'file_size',)
 
     def layer(self, obj):
         return obj.layer_in_dataset.layer.name
