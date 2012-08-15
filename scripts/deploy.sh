c10aedf550cc9350d1381c2fa64aba83a32d49eb
Stephen Davis <steve@koordinates.com>
2012-08-15 15:21:37 +1200

    deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ssh otm@waipoua sudo -u linz2osm "whoami"
+ssh otm@waipoua sudo -u linz2osm "bash -c 'cd /home/linz2osm/linz2osm/src; git pull'"
+ssh otm@waipoua sudo supervisorctl restart all
