b5cc7e75c96e94fb94292e440f14a26c52443731
Stephen Davis <steve@koordinates.com>
2012-08-20 17:43:51 +1200

    deploy clears staticfiles

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 ssh otm@waipoua sudo -u linz2osm "whoami"
 ssh otm@waipoua sudo -u linz2osm "bash -c 'cd /home/linz2osm/linz2osm/src; git pull'"
-ssh otm@waipoua sudo -u linz2osm "bash -c 'cd /home/linz2osm/linz2osm/src ; source ../bin/activate ; ./manage.py collectstatic --noinput'"
+ssh otm@waipoua sudo -u linz2osm "bash -c 'cd /home/linz2osm/linz2osm/src ; source ../bin/activate ; ./manage.py collectstatic --clear --noinput'"
 ssh otm@waipoua sudo supervisorctl restart all
