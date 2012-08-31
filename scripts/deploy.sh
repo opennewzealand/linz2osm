130cc3f80c0a8464898f64bea61ea66ef78de046
Stephen Davis <steve@koordinates.com>
2012-08-31 16:39:54 +1200

    syncdb when deploying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 
 ssh otm@waipoua sudo -u linz2osm "whoami"
 ssh otm@waipoua sudo -u linz2osm "bash -c 'cd /home/linz2osm/linz2osm/src ; git pull'"
+ssh otm@waipoua sudo -u linz2osm "bash -c 'cd /home/linz2osm/linz2osm/src ; source ../bin/activate ; ./manage.py syncdb'"
 ssh otm@waipoua sudo -u linz2osm "bash -c 'cd /home/linz2osm/linz2osm/src ; source ../bin/activate ; ./manage.py migrate'"
 ssh otm@waipoua sudo -u linz2osm "bash -c 'cd /home/linz2osm/linz2osm/src ; source ../bin/activate ; ./manage.py collectstatic --clear --noinput'"
 ssh otm@waipoua sudo supervisorctl restart all
