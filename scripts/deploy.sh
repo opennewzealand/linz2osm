9843518f4cbf626bc7902e8354cecb0bdc2e8d57
Stephen Davis <steve@koordinates.com>
2012-10-11 17:54:47 +1300

    workslice processing restart on deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,3 +6,4 @@ ssh otm@waipoua sudo -u linz2osm "bash -c 'cd /home/linz2osm/linz2osm/src ; sour
 ssh otm@waipoua sudo -u linz2osm "bash -c 'cd /home/linz2osm/linz2osm/src ; source ../bin/activate ; ./manage.py migrate'"
 ssh otm@waipoua sudo -u linz2osm "bash -c 'cd /home/linz2osm/linz2osm/src ; source ../bin/activate ; ./manage.py collectstatic --clear --noinput'"
 ssh otm@waipoua sudo supervisorctl restart all
+ssh otm@waipoua sudo -u linz2osm "bash -c 'cd /home/linz2osm/linz2osm/src ; source ../bin/activate ; ./manage.py restart_processing'"
