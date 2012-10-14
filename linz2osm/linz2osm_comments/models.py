c385da52da66756b56b95d0ebe92ec5b4ce7ed88
Stephen Davis <steve@koordinates.com>
2012-10-15 12:19:32 +1300

    show username as well as full name for #90

--- linz2osm/linz2osm_comments/models.py
+++ linz2osm/linz2osm_comments/models.py
@@ -5,6 +5,15 @@ class AuthenticatedComment(Comment):
     def content_object_link_text(self):
         return ("#%s" % self.object_pk)
 
+    def display_name(self):
+        u = self.user
+        if u:
+            fn = u.get_full_name()
+            if fn:
+                return "%s (%s)" % (u.username, fn)
+            else:
+                return u.username
+
 class WorksliceComment(AuthenticatedComment):
     changesets = models.TextField(blank=True)
 
