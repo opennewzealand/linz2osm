c05364232f5691b2abd583f2aa0e1829d4a41d39
Stephen Davis <steve@koordinates.com>
2012-11-01 17:05:39 +1300

    use changeset service for load_lds_dataset.sh

--- scripts/load_lds_dataset.sh
+++ scripts/load_lds_dataset.sh
@@ -18,25 +18,55 @@
 
 if [ $# -lt 3 ]
 then
-    echo "Usage: load_lds_dataset.sh <database> <layer_id> <layer_name> [<filter>]"
+    echo "Usage: load_lds_dataset.sh {update|overwrite} <database> <layer_id> <layer_name> [<viewparams> [<filter>]]"
     exit 1
 fi
 
-dataset_db=$1
-layer_name=$3
-layer_id="v:x$2"
-wfs_server="http://wfs.data.linz.govt.nz/cd067d1e5bd54e42bacfb9c19942737d/v/x${2}/wfs?SERVICE=WFS&VERSION=1.1.0&TYPENAME=${layer_id}"
-if [ "${4}" ]
+# viewparams are e.g. from:2012-10-12T20:22:22.940122;to:2012-10-26T20:24:26.570496
+
+dataset_db=$2
+layer_name=$4
+layer_id="v:x${3}-changeset"
+wfs_server="http://wfs.data.linz.govt.nz"
+lds_key="cd067d1e5bd54e42bacfb9c19942737d"
+#wfs_cmd="${wfs_server}/${lds_key}/v/x${3}-changeset/wfs?SERVICE=WFS&VERSION=1.1.0&TYPENAME=${layer_id}"
+wfs_cmd="${wfs_server}/${lds_key}/v/x${3}-changeset/wfs?SERVICE=WFS&VERSION=1.1.0&typeName=${layer_id}"
+
+# Add <viewparams>
+if [ "${5}" ]
     then
-    wfs_server="${wfs_server}&CQL_FILTER=${4}"
+    wfs_cmd="${wfs_cmd}&viewparams=${5}"
 fi
-srs="EPSG:4326"
+
+# Add <filter>
+if [ "${6}" ]
+    then
+    wfs_cmd="${wfs_cmd}&CQL_FILTER=${6}"
+fi
+
+
+# Add -lco OVERWRITE=yes if doing overwrite
+lcopts="-lco LAUNDER=yes"
+if [ "${1}" == "overwrite" ]
+    then
+    lcopts="${lcopts} -lco OVERWRITE=yes"
+fi
+
+#srs="EPSG:4326"
+#srs_options="-a_srs ${srs} -s_srs ${srs}"
+srs_options=""
 
 table_name=`echo ${layer_name} | sed -e 's/\(.\)/\L\1/g' -e 's/\([^a-z0-9]\)/_/g'`
-echo "Using server ${wfs_server}"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "Using server ${wfs_cmd}"
 echo "Loading layer: ${layer_name} into ${table_name} in ${dataset_db}"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo 
 # dropdb ${dataset_db}
 # createdb ${dataset_db} -T template_postgis
 
 # FIXME: update, not overwrite
-ogr2ogr -overwrite -f PostgreSQL PG:dbname=${dataset_db} -lco OVERWRITE=yes -lco LAUNDER=yes "WFS:${wfs_server}" -progress -a_srs ${srs} -s_srs ${srs} -nln ${table_name} 
+echo "ogr2ogr -${1} -f PostgreSQL PG:dbname=${dataset_db} ${lcopts} \"WFS:${wfs_cmd}\" ${srs_options} -nln ${table_name} "
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo 
+ogr2ogr -${1} -f PostgreSQL PG:dbname=${dataset_db} ${lcopts} "WFS:${wfs_cmd}" ${srs_options} -nln ${table_name} 
